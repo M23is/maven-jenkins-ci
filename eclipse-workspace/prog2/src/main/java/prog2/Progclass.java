139eb6c1af7f080510e94fc2a1118103a1491189
Misba <misbasaba31@gmail.com>
2025-05-28 16:48:23 +0530

    Initial commit

---
+++ eclipse-workspace/prog2/src/main/java/prog2/Progclass.java
@@ -0,0 +1,10 @@
+package prog2;
+
+public class Progclass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World 🐵🐺");
+		System.out.println("Hello Continous Integration pipeline!!");
+
+	}
+}
